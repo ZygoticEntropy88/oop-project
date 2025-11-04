a8eaf758a5a5229314dcc20a25014056a6a3fa79
ZygoticEntropy <corbalandeconpablo.uni@gmail.com>
2025-11-04 13:00:22 +0100

    Paquete persistencia implementado

--- source/persistencia/gestor_persistencia.py
+++ source/persistencia/gestor_persistencia.py
@@ -3,20 +3,88 @@ import csv
 
 class GestorPersistencia:
 
-    def guardar_json(self, contenido, ruta):
-        pass
+    @staticmethod
+    def guardar_json(self, contenido:dict, ruta:str) -> bool:
+        """Dada la ruta de un archivo .json, guarda el contenido que recibe por parámetros en esa ruta."""
+        try:
+            with open(ruta, "w") as f:
+                json.dump(contenido, f)
 
-    def guardar_csv(self, contenido, ruta):
-        pass
+        except Exception as e:
+            print(f"No se ha podido guardar en el JSON: {ruta}, error {e}")
+            return False
+        return True
 
-    def guardar_txt(self, contenido, ruta):
-        pass
+    @staticmethod
+    def guardar_csv(self, contenido:dict, ruta:str) -> bool:
+        """Dada la ruta de un archivo .csv, guarda el contenido que recibe por parámetros en esa ruta.
+        Para que el método funcione, el contenido debe ser un diccionario de la forma:
+            {'nombre de la cabecera': 'valor de la cabecera',}
+        """
+        try:
+            with open(ruta, "w") as f:
+                cabecera = contenido.keys()
+                escritor = csv.DictWriter(f, fieldnames=cabecera)
+                # Si la cabecera fuera diferente, la machacaría
+                escritor.writeheader()
+                escritor.writerow(contenido)
 
-    def cargar_json(self, ruta, objeto):
-        pass
+        except Exception as e:
+            print(f"No se ha podido escribir en el CSV: {ruta}, error {e}")
+            return False
+        return True
 
-    def cargar_csv(self, ruta, objeto):
-        pass
+    @staticmethod
+    def guardar_txt(self, contenido:str, ruta:str) -> bool:
+        """Dada la ruta de un archivo .txt, guarda el contenido que recibe por parámetros en esa ruta."""
+        try:
+            with  open(ruta), "w" as f:
+                f.write(contenido)
+        except Exception as e:
+            print(f"No se ha podido guardar en el TXT: {ruta}, error {e}")
+            return False
+        return True
 
-    def cargar_txt(self, ruta, objeto):
-        pass
+    @staticmethod
+    def cargar_json(self, ruta:str, objeto:object) -> bool:
+        """Dada la ruta de un archivo .json, y un objeto objeto que debe heredar de IPersistencia, carga su contenido
+        en el archivo .txt. Devuelve True si ha conseguido cargar el contenido satisfactoriamente y false si no puede."""
+        try:
+            with open(ruta, "r") as f:
+                contenido = json.load(f)
+        except Exception as e:
+            print(f"No se ha podido cargar el JSON: {ruta}, error {e}")
+            return False
+        else:
+            objeto.diccionario_a_objeto(contenido)
+            return True
+
+    @staticmethod
+    def cargar_csv(self, ruta:str, objeto:object) -> bool:
+        """Dada la ruta de un archivo .csv, y un objeto objeto que debe heredar de IPersistencia, carga su contenido
+        en el archivo .txt. Devuelve True si ha conseguido cargar el contenido satisfactoriamente y false si no puede."""
+        try:
+            with open(ruta, newline='', encoding='utf-8') as f:
+                lector = csv.DictReader(f)
+                # Convierto el iterador "lector" en una lista de diccionarios de la forma {'cabecera': 'valor'}
+                contenido = list(lector)
+        except Exception as e:
+            print(f"No se ha podido cargar el CSV: {ruta}, error {e}")
+            return False
+        else:
+            objeto.csv_a_objeto(contenido)
+            return True
+
+    @staticmethod
+    def cargar_txt(self, ruta:str, objeto:object) -> bool:
+        """Dada la ruta de un archivo .txt, y un objeto objeto que debe heredar de IPersistencia, carga su contenido
+        en el archivo .txt. Devuelve True si ha conseguido cargar el contenido satisfactoriamente y false si no puede."""
+        try:
+            with open(ruta) as f:
+                contenido = f.read()
+        except Exception as e:
+            print(f"No se ha cargado el texto en el TXT: {ruta}")
+            return False
+        else:
+            objeto.texto_a_objeto(contenido)
+            return True
--- source/persistencia/interfaz_persistencia.py
+++ source/persistencia/interfaz_persistencia.py
@@ -4,24 +4,36 @@ class IPersistencia(ABC):
 
     @abstractmethod
     def objeto_a_texto(self) -> str:
+        """Dado un objeto (aquel que implementa la Interfaz de Persistencia), este método ha de sintetizar los atributos
+        del objeto en una cadena de texto."""
         pass
 
     @abstractmethod
     def objeto_a_diccionario(self) -> dict:
+        """Dado un objeto (aquel que implementa la Interfaz de Persistencia), este método ha de sintetizar los atributos
+        del objeto y devolver un diccionario de la forma {'nombre del atributo' : 'valor del atributo'}"""
         pass
 
     @abstractmethod
     def objeto_a_csv(self) -> dict:
+        """Dado un objeto (aquel que implementa la Interfaz de Persistencia), este método ha de sintetizar los atributos
+        del objeto y devolver un diccionario de la forma {'cabecera' : ' valor del atributo asociado a esa cabecera'}"""
         pass
 
     @abstractmethod
-    def texto_a_objeto(self) -> None:
+    def texto_a_objeto(self, texto:str) -> None:
+        """Dado un texto, el objeto que implemente este método debe extraer del texto los valores necesarios para
+        establecerlos en su propia instancia."""
         pass
 
     @abstractmethod
-    def diccionario_a_objeto(self) -> None:
+    def diccionario_a_objeto(self, diccionario:dict) -> None:
+        """Dado un diccionario, el objeto que implemente este método debe extraer del diccionario los valores necesarios
+        para establecerlos en su propia instancia. """
         pass
 
     @abstractmethod
     def csv_a_objeto(self) -> None:
+        """Dado un csv (que realmente no será más que un diccionario de la forma {'cabecera': 'valor de la cabecera'})
+        este método debe extraer del diccionario los valores necesarios para establecerlos en su propia instancia"""
         pass
