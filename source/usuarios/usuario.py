5da5425410b0ec44b53a284124a6fa69a8718c4b
roctiv22 <victormn06@gmail.com>
2025-11-04 13:04:46 +0100

    Eat cock

--- source/usuarios/usuario.py
+++ source/usuarios/usuario.py
@@ -1,4 +1,40 @@
 from source.persistencia import IPersistencia
 
+class Fecha:
+    def __init__(self, dia : int, mes : int, anyo : int):
+        assert (0 < dia <= 31
+                and 0 < mes <= 12
+                and 0 < anyo <= 2025), "La fecha introducida no es válida"
+
+        self._dia = dia
+        self._mes = mes
+        self._anyo = anyo
+
+    def get_dia(self):
+        return self._dia
+
+    def get_mes(self):
+        return self._mes
+
+    def get_anyo(self):
+        return self._anyo
+
+    def fecha_to_list(self) -> list[int]:
+        fecha : list[int] = [self.get_dia(),
+                             self.get_mes(),
+                             self.get_anyo()]
+        return fecha
+
+
 class Usuario(IPersistencia):
-    pass
+    def __init__(self, nombre_de_usuario : str,
+                 fecha_nacimiento : Fecha,
+                 correo_electronico : str,
+                 contrasenya : str,
+                 fecha_registro : Fecha):
+
+        self._nombre_de_usuario = nombre_de_usuario
+        self._fecha_de_nacimiento = fecha_nacimiento
+        self._correo_electronico = correo_electronico
+        self._contrasenya = contrasenya
+        self.fecha_de_registro = fecha_registro
--- source/usuarios/usuario_premium.py
+++ source/usuarios/usuario_premium.py
@@ -1,4 +1,44 @@
 from .usuario import Usuario
+from .usuario import Fecha
+
+class TarjetaCredito:
+    def __init__(self, numero_de_cuenta : int,
+                 CVV : int,
+                 fecha_de_caducidad : Fecha,
+                 nombre_completo_titular : str,
+                 propietario : UsuarioPremium = None ):
+
+        assert (len(str(numero_de_cuenta)) == 16 and
+                len(str(CVV)) == 3), "Los datos introducidos no son válidos"
+
+        self._numero_de_cuenta = numero_de_cuenta
+        self._CVV = CVV
+        self._fecha_de_caducidad = fecha_de_caducidad
+        self.nombre_completo_titular = nombre_completo_titular
+        self._propietario = propietario
+
+    def get_numero_cuenta (self):
+        return self._numero_de_cuenta
+
+    def get_CVV (self):
+        return self._CVV
+
+    def get_fecha_caducidad (self):
+        return self._fecha_caducidad
+
+    def get_nombre_titular (self):
+        return self._nombre_completo_titular
+
 
 class UsuarioPremium(Usuario):
-    pass
+    def __init__(self, nombre_usuario : str,
+                 fecha_nacimiento : Fecha,
+                 correo_electronico : str,
+                 contrasenya : str,
+                 fecha_registro : Fecha,
+                 tarjeta_credito : TarjetaCredito
+    ):
+        super.__init__(nombre_usuario = nombre_usuario,
+                       fecha_nacimiento = fecha_nacimiento,
+                       correo_electronico = correo_electronico,
+                       
